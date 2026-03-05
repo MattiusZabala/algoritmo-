5b94b9ed869a7ff00f0e2c8133cc3f31a49cf728
MattiusZabala <mattiu.zabala00@usc.edu.co>
2026-03-05 11:53:58 -0500

    factorial.java

--- factorial.java
+++ factorial.java
@@ -4,21 +4,27 @@ public class factorial {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         while (true) {
-            
-        
-        
-        System.out.print("Ingresa un número entero: ");
-        int n = leer.nextInt();
-        long f = 1; 
 
-        if (n < 0) {
-            System.out.println("no se puede calcular el factorial.");
-        } else {
-            for (int i = 1; i <= n; i++) {
-                f *= i; 
+            System.out.print("Ingresa un número entero: ");
+            int n = leer.nextInt();
+
+            if (n < 0) {
+                System.out.println("no se puede calcular el factorial.");
+            } else {
+                long resultado = calculo_factorial(n);
+                System.out.println("El factorial de " + n + " es: " + resultado);
             }
-            System.out.println("El factorial de " + n + " es: " + f);
-        }
         }
     }
+
+    static long calculo_factorial(int n) {
+        long f = 1;
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        return f;
+
+    }
 }
+
+
